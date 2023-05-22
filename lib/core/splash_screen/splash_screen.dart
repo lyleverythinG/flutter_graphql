2b92cc4632cf6c50ae8a292d6db5957338b94cc4
lyleverythinG <lylecarcedo@gmail.com>
2023-05-22 22:48:12 +0800

    Project Architecture Finished.
    - Project Architecture configured.
    - Ready to start working with UI & functionalities.

---
+++ lib/core/splash_screen/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:flutter_graphql/features/book/presentation/pages/home.dart';
+import 'package:lottie/lottie.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  late final Timer _timer;
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer(const Duration(seconds: 3), () async {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (c) => const HomeScreen()));
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SizedBox(
+          child: Lottie.asset(
+            'assets/splash_book.json',
+            repeat: false,
+          ),
+        ),
+      ),
+    );
+  }
+}
