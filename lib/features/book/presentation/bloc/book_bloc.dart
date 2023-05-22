0db6d2f65a3f27c68bb0ac0b155df16590f0e552
lyleverythinG <lylecarcedo@gmail.com>
2023-05-22 22:42:32 +0800

    Created Book Bloc
    - Added bloc template just yet.

---
+++ lib/features/book/presentation/bloc/book_bloc.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:meta/meta.dart';
+part 'book_event.dart';
+part 'book_state.dart';
+
+class BookBloc extends Bloc<BookEvent, BookState> {
+  BookBloc() : super(BookInitial()) {
+    on<BookEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
