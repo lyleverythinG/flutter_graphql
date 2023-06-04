1dbe23ed07bccba169829f230854ab991412d402
lyleverythinG <lylecarcedo@gmail.com>
2023-06-04 16:10:30 +0800

    Update Book Functionality Finished
    - Added UI.
    - Added functionality.

---
+++ lib/features/book/presentation/pages/edit_book_info_screen.dart
@@ -0,0 +1,138 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_graphql/core/constants/constants.dart';
+import 'package:flutter_graphql/core/reusables/custom_button.dart';
+import 'package:flutter_graphql/core/reusables/custom_text.dart';
+import 'package:flutter_graphql/features/book/domain/model/book.dart';
+import 'package:flutter_graphql/features/book/presentation/bloc/book_bloc.dart';
+import 'package:flutter_graphql/features/book/presentation/widgets/provide_book_info.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class EditBookInfoScreen extends StatefulWidget {
+  final BookModel bookModel;
+  final int bookIndex;
+  const EditBookInfoScreen({
+    Key? key,
+    required this.bookModel,
+    required this.bookIndex,
+  }) : super(key: key);
+
+  @override
+  State<EditBookInfoScreen> createState() => _EditBookInfoScreenState();
+}
+
+class _EditBookInfoScreenState extends State<EditBookInfoScreen> {
+  late final bookAuthorNameC = TextEditingController();
+  late final bookTitleC = TextEditingController();
+  late final bookPublishedC = TextEditingController();
+
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    if (widget.bookModel.id != null) {
+      bookAuthorNameC.text = widget.bookModel.author;
+      bookTitleC.text = widget.bookModel.title;
+      bookPublishedC.text = widget.bookModel.year.toString();
+    }
+  }
+
+  @override
+  void dispose() {
+    bookAuthorNameC.dispose();
+    bookTitleC.dispose();
+    bookPublishedC.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: const EdgeInsets.all(8),
+        height: MediaQuery.of(context).size.height,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Form(
+                key: _formKey,
+                child: Container(
+                  padding: const EdgeInsets.all(16),
+                  child: Column(
+                    children: [
+                      const CustomText(
+                          text: 'Please Fill - Up Book Information:'),
+                      Constants.gapH16,
+                      const Align(
+                          alignment: Alignment.topLeft,
+                          child: CustomText(text: 'Author Name:')),
+                      ProvideBookInfo(
+                        controller: bookAuthorNameC,
+                        hintText: 'Enter Author Name',
+                        errorText: 'Please enter Author Name',
+                      ),
+                      Constants.gapH4,
+                      const Align(
+                          alignment: Alignment.topLeft,
+                          child: CustomText(text: 'Book Title:')),
+                      ProvideBookInfo(
+                        controller: bookTitleC,
+                        textInputType: TextInputType.number,
+                        hintText: 'Enter Book Title',
+                        errorText: 'Please enter Book Title',
+                      ),
+                      Constants.gapH4,
+                      const Align(
+                          alignment: Alignment.topLeft,
+                          child: CustomText(text: 'Book Published:')),
+                      ProvideBookInfo(
+                        controller: bookPublishedC,
+                        textInputType: TextInputType.number,
+                        hintText: 'Enter Year',
+                        errorText: 'Please enter Year Published',
+                      ),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height * 0.05),
+                      BlocListener<BookBloc, BookState>(
+                        listener: (context, state) {
+                          if (state is BookUpdated) {
+                            Fluttertoast.showToast(
+                              msg: 'Successfully updated book information.',
+                            );
+                          }
+                        },
+                        child: CustomElevatedButton(
+                          text: 'Update Book Info',
+                          onPressed: () {
+                            if (_formKey.currentState!.validate()) {
+                              context.read<BookBloc>().add(
+                                    UpdateBookEvent(
+                                      bookIndex: widget.bookIndex,
+                                      book: BookModel(
+                                          id: widget.bookModel.id,
+                                          author: bookAuthorNameC.text,
+                                          title: bookTitleC.text,
+                                          year: int.parse(bookPublishedC.text)),
+                                    ),
+                                  );
+                              // Hides keyboard upon updating book info.
+                              FocusManager.instance.primaryFocus?.unfocus();
+                            }
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
