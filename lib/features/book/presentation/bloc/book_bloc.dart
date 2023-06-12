a91f8f3de56cbad2b266a3d9dbc23f10ec16c5b4
lyleverythinG <lylecarcedo@gmail.com>
2023-06-12 16:32:35 +0800

    Local data persistence functionality Finished
    - Setup Hive Finished.
    - Functionality Implemented.

--- lib/features/book/presentation/bloc/book_bloc.dart
+++ lib/features/book/presentation/bloc/book_bloc.dart
@@ -1,22 +1,35 @@
 import 'dart:developer' as developer;
 import 'package:equatable/equatable.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_graphql/features/book/data/repository/book_repo.dart';
 import 'package:flutter_graphql/features/book/domain/model/book.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:hive/hive.dart';
 part 'book_event.dart';
 part 'book_state.dart';
 
 class BookBloc extends Bloc<BookEvent, BookState> {
   BookBloc() : super(BookInitial()) {
     List<BookModel> books = [];
+    final booksBox = Hive.box<BookModel>('booksBox');
 
     on<GetBooksFromAPI>((event, emit) async {
       // Get Books Event
       try {
         emit(const LoadingState());
-        books = await BookRepo.getBooks(limit: 5);
+
+        // Gets the list of books from the API if the box is empty, else retrieve and assign it.
+        books = booksBox.isEmpty
+            ? await BookRepo.getBooks()
+            : booksBox.values.toList();
+
+        // If not empty, adds the list of books from API to the box if it's not yet added.
+        if (booksBox.isEmpty && books.isNotEmpty) {
+          for (BookModel book in books) {
+            booksBox.put(book.id, book);
+          }
+        }
+
         emit(BookUpdated(books: books));
       } catch (e) {
         developer.log('Error getting book/s: $e', name: 'GetBooksError');
@@ -32,16 +45,20 @@ class BookBloc extends Bloc<BookEvent, BookState> {
             title: event.bookInfo.title,
             year: event.bookInfo.year);
 
-        // Inserts the newly added book to the end of the list.
-        debugPrint('bookId $bookId');
         if (bookId != null) {
           BookModel currentBook = BookModel(
               id: bookId,
               author: event.bookInfo.author,
               title: event.bookInfo.title,
               year: event.bookInfo.year);
+
+          // Adds the object to the end of the list.
           books.add(currentBook);
+
+          // Saves the object to the box with the bookId as the key.
+          await booksBox.put(bookId, currentBook);
         }
+
         emit(BookUpdated(books: books));
       } catch (e) {
         developer.log('Error adding book: $e', name: 'AddingBookError');
@@ -56,8 +73,12 @@ class BookBloc extends Bloc<BookEvent, BookState> {
           bookModel: event.book,
         );
 
-        // Updates the current book info.
+        // Updates the current object.
         books[event.bookIndex] = event.book;
+
+        // Updates the current object in the box.
+        await booksBox.put(event.book.id, event.book);
+
         emit(BookUpdated(books: books));
       } catch (e) {
         developer.log('Error Updating Book Info: $e',
@@ -73,9 +94,12 @@ class BookBloc extends Bloc<BookEvent, BookState> {
           id: event.bookId,
         );
 
-        // Updates the current book info.
         if (isBookDeleted) {
+          // Removes the object from the list using the index.
           books.removeAt(event.bookIndex);
+
+          // Deletes the object using the key.
+          booksBox.delete(event.bookId);
           Fluttertoast.showToast(msg: 'deleted successfully');
         }
         emit(BookUpdated(books: books));
--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_graphql/core/constants/constants.dart';
 import 'package:flutter_graphql/core/splash_screen/splash_screen.dart';
+import 'package:flutter_graphql/features/book/domain/model/book.dart';
 import 'package:flutter_graphql/features/book/presentation/bloc/book_bloc.dart';
+import 'package:hive_flutter/adapters.dart';
+import 'core/local_storage_adaptor/user_adaptor.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(BookModelAdapter());
+  await Hive.openBox<BookModel>('booksBox');
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
